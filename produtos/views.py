56bf44719fbcec68cb34de85b919a0c068fc3468
PauloK3tchup <passandoalimpo13@gmail.com>
2025-09-11 09:55:00 -0300

    feat: Filtro pro nome de categoria nos produtos

--- produtos/views.py
+++ produtos/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Produto, Categoria
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ProdutoSerializer, CategoriaSerializer
 
 class CategoriaViewSet(viewsets.ModelViewSet):
@@ -9,3 +10,9 @@ class CategoriaViewSet(viewsets.ModelViewSet):
 class ProdutoViewSet(viewsets.ModelViewSet):
   queryset = Produto.objects.all()
   serializer_class = ProdutoSerializer
+  
+  filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+  filterset_fields = ['preco', 'estoque','categoria__nome']  # Filtragem exata por preço e estoque
+  search_fields = ['nome']  # Busca textual
+  ordering_fields = ['nome', 'preco']  # Ordenação
+  ordering = ['id']  # Ordenação padrão
